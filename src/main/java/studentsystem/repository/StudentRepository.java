2a23af85814230b2d40eb206737efc3ac21b3a68
vinihgomes <contatoviniciusgomes@gmail.com>
2022-05-09 19:21:18 -0300

    arquivos

---
+++ src/main/java/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package studentsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import studentsystem.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
---
+++ src/main/java/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package studentsystem.service;
+
+import studentsystem.model.Student;
+import studentsystem.repository.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
